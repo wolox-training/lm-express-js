c29e9658b3c44bcd0b9d1239bff2d7f640fdf597
lnmedrano <lucas.medrano@wolox.com.ar>
2019-05-29 16:28:55 -0300

    user migration

---
+++ app/models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'first_name'
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'last_name'
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { underscored: true }
+  );
+  /* User.associate = function(models) {
+    // associations can be defined here
+  };*/
+  return User;
+};
