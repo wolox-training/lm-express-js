e034f55438de55e3f27f31f7d17783ebf3ce5fb5
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-06-03 13:06:34 -0300

    Extract functionality from users controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,28 +1,14 @@
-const bcrypt = require('bcrypt'),
-  { databaseError, hashError, validationError } = require('../errors'),
+const { validationError } = require('../errors'),
   Users = require('../models').user,
   logger = require('../logger'),
-  saltRounds = 10;
-
-const hashPassword = pass => bcrypt.hash(pass, saltRounds).catch(error => hashError(error.message));
-
-const createUser = (firstName, lastName, email, password) =>
-  Users.findOrCreate({
-    where: { email },
-    defaults: {
-      firstName,
-      lastName,
-      password
-    }
-  }).catch(error => databaseError(error.message));
+  { hashPassword } = require('../helpers/hasher');
 
 exports.signUp = (req, res, next) => {
   const { first_name: firstName, last_name: lastName, email, password } = req.body;
-  logger.info(`Creating user ${firstName}`);
+  logger.info(`Email and password correctly validated. Creating user ${firstName}`);
 
-  logger.info('Email and password correctly validated');
   return hashPassword(password)
-    .then(hash => createUser(firstName, lastName, email, hash))
+    .then(hash => Users.createUser(firstName, lastName, email, hash))
     .then(([user, created]) => {
       if (created) {
         res.status(200).send(`User ${user.firstName} created`);
