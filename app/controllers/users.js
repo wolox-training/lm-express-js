8b013f4689f14f32cab83f33a559146c84821c3a
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-06-04 10:29:00 -0300

    Add endpoint for signin and check valid inputs

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,7 +1,7 @@
 const { validationError } = require('../errors'),
   Users = require('../models').user,
   logger = require('../logger'),
-  { hashPassword } = require('../helpers/hasher');
+  { hashPassword, comparePasswords } = require('../helpers/hasher');
 
 exports.signUp = (req, res, next) => {
   const { first_name: firstName, last_name: lastName, email, password } = req.body;
@@ -14,6 +14,30 @@ exports.signUp = (req, res, next) => {
         res.status(200).send(`User ${user.firstName} created`);
       } else {
         throw validationError('Email already used');
+      }
+    })
+    .catch(next);
+};
+
+const getUserPassword = user => {
+  if (user) {
+    return user.password;
+  }
+  throw validationError('Email does not exist');
+};
+
+exports.signIn = (req, res, next) => {
+  const { email, password } = req.body;
+  logger.info(`Signing in - email: ${email}`);
+
+  return Users.findUser(email)
+    .then(foundUser => getUserPassword(foundUser))
+    .then(foundUserPassword => comparePasswords(password, foundUserPassword))
+    .then(result => {
+      if (result) {
+        res.status(200).send('token');
+      } else {
+        throw validationError('Password does not match with the email');
       }
     })
     .catch(next);
--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,15 @@
 // const controller = require('./controllers/controller');
 const { getAlbums, getAlbumPhotos } = require('./controllers/albums'),
   { healthCheck } = require('./controllers/healthCheck'),
-  { signUp } = require('./controllers/users'),
-  { checkValidInputs } = require('../app/middlewares/signup');
+  { signUp, signIn } = require('./controllers/users'),
+  { checkValidEmailAndPassword, checkValidName } = require('../app/middlewares/signIn');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', getAlbums);
   app.get('/albums/:id/photos', getAlbumPhotos);
-  app.post('/users', [checkValidInputs], signUp);
+  app.post('/users', [checkValidName, checkValidEmailAndPassword], signUp);
+  app.post('/users/sessions', [checkValidEmailAndPassword], signIn);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
