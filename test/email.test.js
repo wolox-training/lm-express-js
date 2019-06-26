2886a7363f0f6e63f8b6bbace55e0857d406c906
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-06-26 11:06:18 -0300

    Finish tests

--- test/email.test.js
+++ test/email.test.js
@@ -1,18 +1,15 @@
 const { notifySignUp } = require('../app/helpers/mailer'),
   request = require('supertest'),
-  app = require('../app.js'),
-  nodemailer = require('nodemailer'),
-  stubTransport = require('nodemailer-stub-transport');
+  app = require('../app.js');
 
-jest.mock('../app/helpers/mailer');
-notifySignUp.mockResolvedValue(true);
 describe('Test for email-notifying users on sign up', () => {
   beforeEach(() => notifySignUp.mockClear());
+  notifySignUp.mockResolvedValueOnce(true);
   test('Send email', () =>
     notifySignUp('fn', 'email@wolox.com.ar', 'from@wolox.com.ar').then(() => {
       expect(notifySignUp.mock.calls.length).toBe(1);
     }));
-
+  notifySignUp.mockResolvedValueOnce(true);
   test('Create user and sign up', () =>
     request(app)
       .post('/users')
@@ -25,12 +22,4 @@ describe('Test for email-notifying users on sign up', () => {
       .then(() => {
         expect(notifySignUp.mock.calls.length).toBe(1);
       }));
-
-  test('Sen email and check email data', () => {
-    notifySignUp.mockRestore();
-    const transport = nodemailer.createTransport(stubTransport());
-    return notifySignUp('fn', 'email@wolox.com.ar', 'from@wolox.com.ar', transport).then(response => {
-      console.log(response);
-    });
-  });
 });
