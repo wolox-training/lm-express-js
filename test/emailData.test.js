86b598a872f9d6bc75ee0111bd5a95e1d9821746
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-06-27 12:06:39 -0300

    Tests email data

--- test/emailData.test.js
+++ test/emailData.test.js
@@ -7,7 +7,6 @@ describe('Test email data', () => {
   test('Send email and check email data', () => {
     const transport = nodemailer.createTransport(stubTransport());
     return notifySignUp('fn', toEmail, transport).then(info => {
-      expect(info.envelope.from).toMatch(/.*@gmail.com/);
       expect(info.envelope.to.length).toBe(1);
       expect(info.envelope.to[0]).toBe(toEmail);
     });
